2be631572670a56096c4e61eedf03bfdfdbe01de
Crystal Meneses <Crystal@Crystals-MacBook-Air.local>
2014-02-12 15:05:19 -0500

    Fixed bug with user profile validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
    # Setup accessible (or protected) attributes for your model
    attr_accessible :email, :password, :password_confirmation, :remember_me,
                    :first_name, :last_name, :profile_name
-   # attr_accessible :title, :body
+   # attr_accessible :title, :body`
 
    validates :first_name, presence: true
 
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
    validates :profile_name, presence: true,
                             uniqueness: true,
                             format: {
-                            	with: /a-zA-Z0-9_-/,
+                            	with: /\A[a-zA-Z0-9_-]+\Z/,
                             	message: 'Must be formatted correctly.'
                             }
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,11 +32,21 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-  	user = User.new
+    user = User.new(first_name: 'Crystal', last_name: 'Meneses', email: 'crystalm@gmail.com')
+    user.password = user.password_confirmation = 'LillyA'
   	user.profile_name = "My profile with spaces"
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  test "a user should have a correctly formatted profile name" do
+    user = User.new(first_name: 'Crystal', last_name: 'Meneses', email: 'crystalm@gmail.com')
+    user.password = user.password_confirmation = 'LillyA'
+
+
+    user.profile_name = 'crystalmeneses_1'
+    assert !user.valid?
   end
 end
